892e205139e5402465b670c42c5ffada3722c30c
ZamCo2 <51409607+ZamCo2@users.noreply.github.com>
2019-06-29 21:04:02 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 var myImage = document.querySelector('img');
 
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h3');
+
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/brockhampton.jpeg') {
@@ -7,4 +10,21 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/brockhampton.jpeg');
     }
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome, ' + myName + ' :)';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome, ' + storedName +' :)';
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
